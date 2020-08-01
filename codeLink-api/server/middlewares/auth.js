c7d06e1a94e050c0d9efda0cbb08909c837dea52
oskr814 <oskralvarez814@gmail.com>
2020-08-01 14:32:36 -0600

    [ADD: Backend] Bearer JWT validation

--- codeLink-api/server/middlewares/auth.js
+++ codeLink-api/server/middlewares/auth.js
@@ -3,11 +3,16 @@ const jwt = require("jsonwebtoken");
 let verificarToken = (req, res, next) => {
   let token = req.get("token");
 
-  if(token.startsWith('Bearer ')) {
-    token = token.slice(7, token.length);
+  if (!token) {
+    return res.status(401).json({
+      ok: false,
+      message: "Debe proporcionar el token",
+    });
   }
 
-  console.log(token);
+  if (token.startsWith("Bearer ")) {
+    token = token.slice(7, token.length);
+  }
 
   jwt.verify(token, process.env.SEED, (err, decoded) => {
     if (err) {
