abae838048f7a71cfb36c4be8808acdae28ae809
oskr814 <oskralvarez814@gmail.com>
2020-08-18 18:35:25 -0600

    [ADD] Prod Build Config

--- codeLink-api/server/server.js
+++ codeLink-api/server/server.js
@@ -1,19 +1,30 @@
-require('./config/config');
+require("./config/config");
 
-const express = require('express');
+const express = require("express");
 const database = require('./config/database');
+const path = require("path");
 
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(require('./routes/index-route'));
+app.use(require("./routes/index-route"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.send("Ruta no valida");
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
 app.listen(process.env.PORT, () => {
-    console.log('Servidor inicado en el puerto 3000');
+  console.log("Servidor inicado en el puerto 3000");
 });
